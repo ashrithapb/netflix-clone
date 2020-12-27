6d1f97a1ff129b0e63cee09f8e70c587048fbc0d
Ashritha <ashrithapb@gmail.com>
2020-12-27 10:29:51 -0600

    first commit

---
+++ src/Row.js
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from "react";
+import "./Row.css";
+import axios from "./axios";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
+
+const base_url = "https://image.tmdb.org/t/p/original/";
+
+function Row({ title, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
+  useEffect(() => {
+    async function fetchData() {
+      const requests = await axios.get(fetchUrl);
+      setMovies(requests.data.results);
+      return requests;
+    }
+    fetchData();
+  }, [fetchUrl]);
+
+  const opts = {
+    height: "390",
+    width: "640",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+  /*const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };*/
+  const handleClick = async (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      let trailerurl = await axios.get(
+        `/movie/${movie.id}/videos?api_key=fb34530271b349314af0de263d16ab5a`
+      );
+      console.log("test " + trailerurl);
+      setTrailerUrl(trailerurl.data.results[0]?.key);
+    }
+  };
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            onClick={() => handleClick(movie)}
+            className={`row__poster ${isLargeRow && "row__posterLarger"}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+      <div className="row__trailer">
+        {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
+      </div>
+    </div>
+  );
+}
+
+export default Row;
